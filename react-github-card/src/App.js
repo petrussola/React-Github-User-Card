9531298aa0c5eb4b737b57780c1faad578d84536
Pere <pere.solaclaver@gmail.com>
2019-10-01 13:43:31 +0100

    Pass props data to the user component

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <UserCard />
+        <UserCard user={this.state.user} />
         <FollowersCard />
       </div>
     );
--- react-github-card/src/components/UserCard.js
+++ react-github-card/src/components/UserCard.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default class UserCard extends React.Component {
     render() {
+        debugger
         return (
-            <div>In the User Card</div>
+            <div>
+                <div>
+                    <img src={this.props.user.avatar_url} />
+                </div>
+                <p>{this.props.user.login}</p>
+                <p>{this.props.user.name}</p>
+                <p>{this.props.user.bio}</p>
+                <p>{this.props.user.followers}</p>
+                <p>{this.props.user.following}</p>
+            </div>
         )
     }
 }
