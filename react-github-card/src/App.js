87a438612e780f869075f0eef3fb11e00613c796
Pere <pere.solaclaver@gmail.com>
2019-10-01 13:34:04 +0100

    Fetch data from github api with axios

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 import UserCard from "./components/UserCard";
 import FollowersCard from "./components/FollowersCard";
+
+const userApi = "https://api.github.com/users/petrussola";
+const followersApi = "https://api.github.com/users/petrussola/followers";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,6 +15,19 @@ class App extends React.Component {
       user: {},
       followers: []
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get(userApi)
+      .then(res => {
+        this.setState({
+          user: res.data,
+        })
+      })
+      .catch(error => {
+        debugger;
+      });
   }
 
   render() {
