02a64a2cf96241f35e6c10c3ff54cf9f4de4ba80
Pere <pere.solaclaver@gmail.com>
2019-10-01 14:46:43 +0100

    Style followers card

--- react-github-card/src/components/FollowerCard.js
+++ react-github-card/src/components/FollowerCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledFollowerCard = styled.p`
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 0.5rem;
+    margin: 0.2rem;
+`;
 
 export default class FollowerCard extends React.Component {
     render() {
         return (
-            <p>{this.props.followers.login}</p>
+            <StyledFollowerCard>{this.props.followers.login}</StyledFollowerCard>
         )
     }
 }
--- react-github-card/src/components/Followers.js
+++ react-github-card/src/components/Followers.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import FollowerCard from './FollowerCard';
+import React from "react";
+import styled from "styled-components";
+
+import FollowerCard from "./FollowerCard";
+
+const StyledFollowers = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 export default class UserCard extends React.Component {
-    render() {
-        return (
-            <div className='section'>
-                Followers: 
-                {
-                    this.props.followers.map( item => {
-                        return ( <FollowerCard followers={item} />)
-                    })
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sectionFollowers">
+        Followers:
+        <StyledFollowers >
+          {this.props.followers.map(item => {
+            return <FollowerCard followers={item} />;
+          })}
+        </StyledFollowers>
+      </div>
+    );
+  }
 }
